9ce4e9fb9f007bb998a2a0924c7c9d26545343fb
Moritz E. Beber <midnighter@posteo.net>
2024-04-21 16:17:30 +0200

    feat: implement function skeleton

---
+++ R/load.R
@@ -0,0 +1,13 @@
+#' Load BIOM format file to TreeSummarizedExperiment
+#'
+#' @param filename A character vector with one element that denotes the file path
+#'    to a BIOM file.
+#'
+#' @return A TreeSummarizedExperiemnt object
+#' @export
+#'
+#' @examples
+#' tse <- load_biom("profiles.biom")
+load_biom <- function(filename) {
+  return()
+}
